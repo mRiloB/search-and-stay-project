2cb39287418c1956850541606b720bd948a7ed54
Murilo Benício <m.benicio@outlook.com>
2023-05-26 12:11:21 -0300

    rules list

---
+++ src/components/RuleListItem.vue
@@ -0,0 +1,20 @@
+<script lang='ts' setup>
+defineProps<{
+  text: string
+  id: number
+}>()
+</script>
+
+<template>
+  <q-item>
+    <q-item-section>
+      {{ text }}
+    </q-item-section>
+
+    <q-item-section side>
+      <q-btn size="12px" flat dense round icon="more_vert" />
+    </q-item-section>
+  </q-item>
+</template>
+
+<style lang='scss' scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
-// import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useHouseRules } from 'src/services/house-roules'
+import { HouseRule } from 'src/types/house-rule'
+import RuleListItem from 'src/components/RuleListItem.vue'
+
+onMounted(() => list())
+
+const houseRules = useHouseRules()
+const houseRulesList = ref<HouseRule[]>([])
+const _loading = ref(false)
+
+async function list () {
+  _loading.value = true
+  try {
+    const res = await houseRules._read()
+    houseRulesList.value = res.data.entities
+  } catch (err) {
+    console.log('IndexPageError:', err)
+  } finally {
+    _loading.value = false
+  }
+}
 </script>
 
 <template>
   <q-page padding>
-    <q-list bordered class="rounded-borders">
-      <!-- <q-item-label header>House Rules</q-item-label> -->
-      <q-item>
-        <q-item-section>
-          No Pets
-        </q-item-section>
-
-        <q-item-section side>
-          <q-btn size="12px" flat dense round icon="more_vert" />
-        </q-item-section>
-      </q-item>
+    <p v-if="houseRulesList.length == 0">No rules. Add a new rule in the button bellow.</p>
+    <q-list v-for="(rule, index) in houseRulesList" :key="index" class="rounded-borders q-mb-sm" bordered v-else>
+      <rule-list-item :id="rule.id!" :text="rule.name" />
     </q-list>
+    <q-inner-loading :showing="_loading">
+      <q-spinner-gears size="50px" color="primary" />
+    </q-inner-loading>
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab icon="add" color="primary" />
+    </q-page-sticky>
   </q-page>
 </template>
